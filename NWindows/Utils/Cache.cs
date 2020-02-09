00d4c7cca2ad190196d834c7d3291231810e37cb
Yuriy Kopylov <iyurij@gmail.com>
2020-02-09 15:01:42 +1100

    Implemented Cache class.

---
+++ NWindows/Utils/Cache.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NWindows.Utils
+{
+    internal class Cache<K, V>
+    {
+        private readonly Dictionary<K, LinkedListNode<(K, V)>> nodesByKey;
+        private readonly LinkedList<(K, V)> nodes;
+
+        private readonly int size;
+        private readonly Func<K, V> valueConstructor;
+        private readonly Action<V> evictionCallback;
+
+        public Cache(int size, Func<K, V> valueConstructor, Action<V> evictionCallback, IEqualityComparer<K> comparer)
+        {
+            nodesByKey = new Dictionary<K, LinkedListNode<(K, V)>>(comparer);
+            nodes = new LinkedList<(K, V)>();
+
+            this.size = size;
+            this.valueConstructor = valueConstructor;
+            this.evictionCallback = evictionCallback;
+        }
+
+        public void Clear()
+        {
+            foreach (var value in nodes)
+            {
+                evictionCallback(value.Item2);
+            }
+
+            nodesByKey.Clear();
+            nodes.Clear();
+        }
+
+        public V Get(K key)
+        {
+            if (nodesByKey.TryGetValue(key, out var node))
+            {
+                nodes.Remove(node);
+                nodes.AddLast(node);
+            }
+            else
+            {
+                var value = valueConstructor(key);
+                node = nodes.AddLast((key, value));
+                nodesByKey.Add(key, node);
+            }
+
+            TrimSize();
+
+            return node.Value.Item2;
+        }
+
+        private void TrimSize()
+        {
+            while (nodesByKey.Count > size)
+            {
+                var node = nodes.First;
+                evictionCallback(node.Value.Item2);
+                nodes.Remove(node);
+                nodesByKey.Remove(node.Value.Item1);
+            }
+        }
+    }
+}
---
+++ Test.NWindows/Utils/TestCache.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using NWindows.Utils;
+
+namespace Test.NWindows.Utils
+{
+    public class TestCache
+    {
+        [Test]
+        public void TestGetEvict()
+        {
+            List<string> evicted = new List<string>();
+            var cache = new Cache<int, string>(5, i => i.ToString(), s => evicted.Add(s), EqualityComparer<int>.Default);
+
+            Assert.That(cache.Get(1), Is.EqualTo("1"));
+            Assert.That(cache.Get(2), Is.EqualTo("2"));
+            Assert.That(cache.Get(3), Is.EqualTo("3"));
+            Assert.That(cache.Get(4), Is.EqualTo("4"));
+            Assert.That(cache.Get(5), Is.EqualTo("5"));
+            Assert.That(evicted, Is.Empty);
+
+            Assert.That(cache.Get(6), Is.EqualTo("6"));
+            Assert.That(evicted, Is.EqualTo(new string[] {"1"}));
+
+            Assert.That(cache.Get(2), Is.EqualTo("2"));
+            Assert.That(evicted, Is.EqualTo(new string[] {"1"}));
+
+            Assert.That(cache.Get(7), Is.EqualTo("7"));
+            Assert.That(evicted, Is.EqualTo(new string[] {"1", "3"}));
+
+            cache.Clear();
+            Assert.That(evicted, Is.EquivalentTo(new string[] {"1", "2", "3", "4", "5", "6", "7"}));
+            evicted.Clear();
+
+            Assert.That(cache.Get(11), Is.EqualTo("11"));
+            Assert.That(cache.Get(12), Is.EqualTo("12"));
+            Assert.That(cache.Get(13), Is.EqualTo("13"));
+            Assert.That(cache.Get(14), Is.EqualTo("14"));
+            Assert.That(cache.Get(15), Is.EqualTo("15"));
+            Assert.That(evicted, Is.Empty);
+
+            Assert.That(cache.Get(15), Is.EqualTo("15"));
+            Assert.That(cache.Get(14), Is.EqualTo("14"));
+            Assert.That(cache.Get(13), Is.EqualTo("13"));
+            Assert.That(cache.Get(12), Is.EqualTo("12"));
+            Assert.That(cache.Get(11), Is.EqualTo("11"));
+            Assert.That(evicted, Is.Empty);
+
+            Assert.That(cache.Get(21), Is.EqualTo("21"));
+            Assert.That(cache.Get(22), Is.EqualTo("22"));
+            Assert.That(cache.Get(23), Is.EqualTo("23"));
+            Assert.That(cache.Get(24), Is.EqualTo("24"));
+            Assert.That(cache.Get(25), Is.EqualTo("25"));
+            Assert.That(evicted, Is.EqualTo(new string[] {"15", "14", "13", "12", "11"}));
+        }
+
+        [Test]
+        public void TestExceptionInConstructor()
+        {
+            string ValueConstructor(int i)
+            {
+                if (i % 2 == 0)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return i.ToString();
+            }
+
+            List<string> evicted = new List<string>();
+            var cache = new Cache<int, string>(3, ValueConstructor, s => evicted.Add(s), EqualityComparer<int>.Default);
+
+            Assert.That(cache.Get(1), Is.EqualTo("1"));
+            Assert.That(() => cache.Get(2), Throws.Exception.TypeOf<InvalidOperationException>());
+            Assert.That(() => cache.Get(2), Throws.Exception.TypeOf<InvalidOperationException>());
+            Assert.That(cache.Get(3), Is.EqualTo("3"));
+            Assert.That(() => cache.Get(4), Throws.Exception.TypeOf<InvalidOperationException>());
+            Assert.That(() => cache.Get(4), Throws.Exception.TypeOf<InvalidOperationException>());
+            Assert.That(cache.Get(5), Is.EqualTo("5"));
+            Assert.That(() => cache.Get(2), Throws.Exception.TypeOf<InvalidOperationException>());
+            Assert.That(() => cache.Get(4), Throws.Exception.TypeOf<InvalidOperationException>());
+            Assert.That(() => cache.Get(6), Throws.Exception.TypeOf<InvalidOperationException>());
+            Assert.That(evicted, Is.Empty);
+
+            Assert.That(cache.Get(7), Is.EqualTo("7"));
+            Assert.That(evicted, Is.EqualTo(new string[] {"1"}));
+
+            cache.Clear();
+            Assert.That(evicted, Is.EquivalentTo(new string[] {"1", "3", "5", "7"}));
+            evicted.Clear();
+
+            cache.Clear();
+            Assert.That(evicted, Is.Empty);
+        }
+
+        [Test]
+        public void TestEqualityComparer()
+        {
+            List<string> evicted = new List<string>();
+            var cache = new Cache<int, string>(3, i => i.ToString(), s => evicted.Add(s), Mod10EqualityComparer.Instance);
+
+            Assert.That(cache.Get(11), Is.EqualTo("11"));
+            Assert.That(cache.Get(12), Is.EqualTo("12"));
+            Assert.That(cache.Get(13), Is.EqualTo("13"));
+            Assert.That(evicted, Is.Empty);
+
+            Assert.That(cache.Get(21), Is.EqualTo("11"));
+            Assert.That(cache.Get(22), Is.EqualTo("12"));
+            Assert.That(cache.Get(23), Is.EqualTo("13"));
+            Assert.That(evicted, Is.Empty);
+
+            Assert.That(cache.Get(15), Is.EqualTo("15"));
+            Assert.That(evicted, Is.EqualTo(new string[] {"11"}));
+
+            cache.Clear();
+            Assert.That(evicted, Is.EquivalentTo(new string[] {"11", "12", "13", "15"}));
+        }
+
+        private class Mod10EqualityComparer : IEqualityComparer<int>
+        {
+            public static Mod10EqualityComparer Instance { get; } = new Mod10EqualityComparer();
+
+            private Mod10EqualityComparer() {}
+
+            public bool Equals(int x, int y)
+            {
+                return x % 10 == y % 10;
+            }
+
+            public int GetHashCode(int obj)
+            {
+                return obj % 10;
+            }
+        }
+    }
+}
